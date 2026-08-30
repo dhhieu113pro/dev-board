e88d3cb36ab8b5b621f000d7ead46a8aa9eb752b
Hieu Dam <86464713+dhhieu113pro@users.noreply.github.com>
2026-08-30 16:55:20 +0700

    test: cover worktree name derivation from branch title

---
+++ tests/DevBoard.Tests/AddWorktreeTests.cs
@@ -0,0 +1,16 @@
+using DevBoard.ViewModels;
+
+namespace DevBoard.Tests;
+
+public class AddWorktreeTests
+{
+    [Theory]
+    [InlineData("feat/sprint246/53576-email-tech-change-the-email-queue-to-use-postmarkapp", "53576-email-tech-change-the-email-queue-to-use-postmarkapp")]
+    [InlineData("fix/12345-something", "12345-something")]
+    [InlineData("plain-worktree", "plain-worktree")]
+    [InlineData("feature/nested/name/", "name")]
+    public void DeriveWorktreeName_UsesLastBranchSegment(string title, string expected)
+    {
+        Assert.Equal(expected, AddWorktree.DeriveWorktreeName(title));
+    }
+}

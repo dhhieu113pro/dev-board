deadbb3d7210599072b6f582263534a6c67c318c
Hieu Dam <86464713+dhhieu113pro@users.noreply.github.com>
2026-08-30 14:26:22 +0700

    feat: expose GitHub accounts in preferences

---
+++ src/Views/Preferences.GitHubAccounts.cs
@@ -0,0 +1,36 @@
+using System;
+using System.Linq;
+
+using Avalonia.Controls;
+using Avalonia.VisualTree;
+
+namespace DevBoard.Views
+{
+    public partial class Preferences
+    {
+        protected override void OnOpened(EventArgs e)
+        {
+            base.OnOpened(e);
+
+            var tabs = this.GetVisualDescendants().OfType<TabControl>().FirstOrDefault();
+            if (tabs == null || tabs.Items.OfType<TabItem>().Any(x => x.Tag as string == GitHubAccountsTabTag))
+                return;
+
+            tabs.Items.Add(new TabItem
+            {
+                Tag = GitHubAccountsTabTag,
+                Header = new TextBlock
+                {
+                    Classes = { "tab_header" },
+                    Text = "GitHub Accounts",
+                },
+                Content = new GitHubAccounts
+                {
+                    DataContext = new ViewModels.GitHubAccountsViewModel(),
+                },
+            });
+        }
+
+        private const string GitHubAccountsTabTag = "devboard.github-accounts";
+    }
+}
